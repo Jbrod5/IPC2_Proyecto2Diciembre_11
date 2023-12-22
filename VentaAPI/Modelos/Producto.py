0ba1393d58f3fe0a52dda7425ccd8841ec66ae61
Jbrod5 <jorgebravo202131782@cunoc.edu.gt>
2023-12-22 00:19:17 -0600

    creacion de modelos Cliente, Producto, correccion id en producto

--- VentaAPI/Modelos/Producto.py
+++ VentaAPI/Modelos/Producto.py
@@ -1,6 +1,7 @@
 # Modelo de Producto (controladores en models.py)
 class Producto:
-    def __init__(self, nombre, descripcion, precio, stock):
+    def __init__(self, id, nombre, descripcion, precio, stock):
+        self._id = id
         self._nombre = nombre
         self._descripcion = descripcion
         self._precio = precio
@@ -14,8 +15,12 @@ class Producto:
         return self._precio
     def obtener_stock(self):
         return self._stock
+    def obtener_id(self):
+        return self._id
     
 
+    def establecer_id(self, id):
+        self._id = id
     def establecer_nombre(self, nombre):
         self._nombre = nombre
     def establecer_descripcion(self, descripcion):
